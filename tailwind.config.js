22010821e18a6184aed4aed32883be23f4b73e8a
Jose Pablo Vazquez Garcia <jose.vazquez@alten.es>
2022-04-12 14:25:50 +0200

    Inicio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        "dorado": "F8CB3C",
+        "amarillo-claro": "FCF489",
+        "amarillo-oscuro": "FFE763",
+        "marron": "2C110A"
+      }
+    },
+  },
+  plugins: [],
+}
