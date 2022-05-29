ccbbaee5e2f13a24fab7fc713d17e5867fc9d9f8
Jose Pablo Vazquez Garcia <jose.vazquez@alten.es>
2022-05-30 00:07:27 +0200

    Añadido DarkModeButton component.

---
+++ src/components/DarkModeButton.jsx
@@ -0,0 +1,25 @@
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { MyContext } from './helpers/MyContext'
+
+const DarkModeButton = (props) => {
+    const [{clientBalance, setClientBalance}, {windowWidth, setWindowWidth}, {dark, setDark}] = React.useContext(MyContext);
+
+    const darkMode = () => {
+        setDark(!dark);
+    }
+
+    return (
+        <div className={props.className}>
+            <FontAwesomeIcon icon={faMoon}/>
+            <label className="switch">
+                <input type="checkbox" onClick={darkMode}/>
+                <span className="slider round"/>
+            </label>
+            <FontAwesomeIcon icon={faSun}/>
+        </div>
+  )
+}
+
+export default DarkModeButton
--- src/components/helpers/MyContext.jsx
+++ src/components/helpers/MyContext.jsx
@@ -5,10 +5,11 @@ export const MyContext = React.createContext({})
 export const MyContextProvider = props => {
   const [clientBalance, setClientBalance] = React.useState(0);
   const [windowWidth, setWindowWidth] = React.useState(0);
+  const [dark, setDark] = React.useState(true);
 
   return (
     <MyContext.Provider
-      value={[{clientBalance, setClientBalance}, {windowWidth, setWindowWidth}]}
+      value={[{clientBalance, setClientBalance}, {windowWidth, setWindowWidth}, {dark, setDark}]}
     >
       {props.children}
     </MyContext.Provider>
