cc62261afeb02fd19f0f1b4d51af518034b59023
Jose Pablo Vazquez Garcia <jose.vazquez@alten.es>
2022-05-31 13:18:39 +0200

    Añadido dark mode al Home.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import LogoBlanco from './svg/LogoBlanco';
+import LogoOscuro from './svg/LogoOscuro';
+import { MyContext } from './helpers/MyContext';
 
 const Home = () => {
+  const [{clientBalance, setClientBalance}, {windowWidth, setWindowWidth}, {dark, setDark}] = React.useContext(MyContext);
+
+
   return (
-    <main className='flex flex-col flex-grow items-center bg-marron text-amarillo-claro font-montaga 
-          xl:w-3/4 xl:items-start xl:bg-transparent xl:bg-gradient-to-r from-marron via-marron'>
+    <main className={
+      !dark
+      ? 'flex flex-col flex-grow items-center bg-amarillo-oscuro text-marron font-montaga xl:w-3/4 xl:items-start xl:bg-transparent xl:bg-gradient-to-r from-dorado via-amarillo-claro'
+      : 'flex flex-col flex-grow items-center bg-marron text-amarillo-claro font-montaga xl:w-3/4 xl:items-start xl:bg-transparent xl:bg-gradient-to-r from-marron via-marron'
+    }>
 
       <Link to='/login' className='self-end mr-10 mt-10 text-2xl hover:underline 
                         lg:text-4xl 
@@ -14,17 +22,23 @@ const Home = () => {
       </Link>
 
       <div className='flex flex-col items-center'>
-        <LogoBlanco aspect="w-full max-w-lg mt-16 lg:mt-8"/>
+        {
+          !dark ? <LogoOscuro aspect="w-full max-w-lg mt-16 lg:mt-8"/> : <LogoBlanco aspect="w-full max-w-lg mt-16 lg:mt-8"/>
+        }
 
-        <h1 className='px-5 -mt-6 font-cylburn text-amarillo-claro text-7xl 
-          lg:text-8xl
-          xl:ml-3 xl:-mt-8'>
+        <h1 className={
+          !dark
+          ? 'px-5 -mt-6 font-cylburn text-marron text-7xl lg:text-8xl xl:ml-3 xl:-mt-8'
+          : 'px-5 -mt-6 font-cylburn text-amarillo-claro text-7xl lg:text-8xl xl:ml-3 xl:-mt-8'
+        }>
             Winning Horse
         </h1>
 
-        <p className='px-5 mt-6 text-center text-white text-xl 
-          lg:text-2xl
-          xl:ml-4'>
+        <p className={
+          !dark
+          ? 'px-5 mt-6 text-center text-green-900 text-xl lg:text-2xl xl:ml-4'
+          : 'px-5 mt-6 text-center text-white text-xl lg:text-2xl xl:ml-4'
+        }>
             Manage your horse racing bets the easiest way possible
         </p>
           
